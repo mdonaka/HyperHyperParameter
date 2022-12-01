b6ebe0f7e079a47700708dd736b2ef42d51aa9ff
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2022-12-01 21:16:30 +0900

    Add plot template

--- Visualizer/plot.py
+++ Visualizer/plot.py
@@ -1,7 +1,21 @@
+import csv
+
 import matplotlib.pyplot as plt
 
+
+def read_data(file):
+    with open(file, "r") as f:
+        reader = csv.reader(f)
+        y = [float(t[0]) for t in reader]
+        x = [t+1 for t in range(len(y))]
+        return x, y
+
+
 if __name__ == '__main__':
-    x = [1, 2]
-    y = [1, 2]
-    plt.plot(x, y)
+    x, y = read_data("../Result/f0")
+    plt.yscale('log')
+    plt.plot(x, y, label="hoge", c="b")
+    plt.plot(x, y, label="huga", c="r")
+    plt.legend()
+    plt.grid()
     plt.show()
