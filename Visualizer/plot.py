06c97ad939a86e15a2de16f9c7ccf36d9b7745fd
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2022-12-01 21:22:24 +0900

    Clean plot

--- Visualizer/plot.py
+++ Visualizer/plot.py
@@ -1,4 +1,5 @@
 import csv
+from dataclasses import dataclass
 
 import matplotlib.pyplot as plt
 
@@ -11,11 +12,28 @@ def read_data(file):
         return x, y
 
 
+@dataclass(frozen=True)
+class Data:
+    file: str
+    label: str
+    c: str
+
+
+def plot_data(data: list[Data]):
+    for d in data:
+        x, y = read_data(d.file)
+        plt.plot(x, y, label=d.label, c=d.c)
+
+
 if __name__ == '__main__':
-    x, y = read_data("../Result/f0")
+    data = [
+        Data("../Result/f0", "hoge", "b"),
+        Data("../Result/f0", "hoge", "r"),
+    ]
+
+    plot_data(data)
+
     plt.yscale('log')
-    plt.plot(x, y, label="hoge", c="b")
-    plt.plot(x, y, label="huga", c="r")
     plt.legend()
     plt.grid()
     plt.show()
