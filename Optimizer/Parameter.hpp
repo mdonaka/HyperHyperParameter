b459c801f54d16a5f0c97102acdf9c086e3278a4
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2022-12-05 03:43:26 +0900

    fix parameter

--- Optimizer/Parameter.hpp
+++ Optimizer/Parameter.hpp
@@ -1,5 +1,7 @@
-
+#include <string>
 namespace Param {
 constexpr int NP = 20;
 constexpr int EVAL = 1000;
+// Rastrigin, Rosenbrock, Schwefel
+const std::string FUNC = "Schwefel";
 }  // namespace Param
